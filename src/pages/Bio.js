15b3c518102004bc4f36b8a532be96fe699766cc
ankitOneNineNineNine <pradhanankit12@gmail.com>
2022-01-08 07:25:39 +0545

    page set

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -3,11 +3,16 @@ import React from 'react';
 export default function Bio() {
     return (
         <article data-name="article-full-bleed-background">
-            <div className="cf ph5" style={{background: "url(http://mrmrs.github.io/photos/12.jpg) no-repeat center center fixed", backgroundSize: "cover"}}>
+            <div className="cf ph5" style={{ background: "url(/GYM1.jpg) no-repeat center center fixed", backgroundSize: "cover" }}>
                 <div className="fl pa3 pa4-ns bg-white black-70 measure-narrow f3 times">
-                    <header className="bb b--black-70 pv4">
-                        <h3 className="f2 fw7 ttu tracked lh-title mt0 mb3 avenir">My Bio!</h3>
-                        <h4 className="f3 fw4 i lh-title mt0">Bhaktapur, Nepal</h4>
+                    <header className="flex bb b--black-70 pv4 justify-between">
+                        <div>
+                            <h3 className="f2 fw7 ttu tracked lh-title mt0 mb3 avenir">My Bio!</h3>
+                            <h4 className="f3 fw4 i lh-title mt0">Bhaktapur, Nepal</h4>
+                        </div>
+                        <div className='w-30'>
+                            <img className='br-pill' alt='trainer image' src='/trainer.jpg' />
+                        </div>
                     </header>
                     <section className="pt5 pb4">
                         <p className="times lh-copy measure f4 mt0">
@@ -35,9 +40,9 @@ export default function Bio() {
                             Come train with me once and you wont need no trainer for life as I will not only improve your physical conditions but also
                             teach you about doing what is beneficial for you body.
                         </p>
-                        
-              
-            </section>
+
+
+                    </section>
                 </div>
             </div>
         </article>
